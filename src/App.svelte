81fff32f4b2c76084a225567695d6e4432f3c797
Rizqy EP <rizqyepr@gmail.com>
2021-07-23 01:57:51 +0700

    feat: scaffolded main components

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,34 @@
 <script>
-	export let name;
+  export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos,
+    reiciendis est corporis possimus aperiam natus voluptatem neque deleniti
+    minus fugiat molestias dolores voluptas? Alias et molestiae nulla facere
+    ipsa totam!
+  </p>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
