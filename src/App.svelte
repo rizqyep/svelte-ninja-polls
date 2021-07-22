2c5f83a9d4ee38ccb8ecd1d3ee3e9d33f4ae773f
Rizqy EP <rizqyepr@gmail.com>
2021-07-23 02:03:56 +0700

    feat: header footer scaffold

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,8 @@
 <script>
-  export let name;
+  import Header from "./components/Header.svelte";
 </script>
 
+<Header />
 <main>
   <p>
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos,
@@ -15,20 +16,7 @@
   main {
     text-align: center;
     padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
+    max-width: 960px;
+    margin: 40px auto;
   }
 </style>
---
+++ src/components/Footer.svelte
@@ -0,0 +1,3 @@
+<footer>
+  <div class="copyright">Copyright 2021 - Rep the Ninja</div>
+</footer>
