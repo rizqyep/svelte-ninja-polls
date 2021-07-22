9a477a9ff44628294ab76fd2eb702410abe736d2
Rizqy EP <rizqyepr@gmail.com>
2021-07-23 02:56:19 +0700

    feat: added form validation

--- src/components/CreatePollForm.svelte
+++ src/components/CreatePollForm.svelte
@@ -1,9 +1,43 @@
 <script>
+  import Button from "./../shared/Button.svelte";
   let fields = { question: "", answerA: "", answerB: "" };
 
+  let errors = { question: "", answerA: "", answerB: "" };
+
+  let valid = false;
+
   const submitHandler = () => {
-    console.log(fields);
+    valid = true;
+
+    //Validate Question
+    if (fields.question.trim().length < 5) {
+      valid = false;
+      errors.question = "Question need to be 5 letter minimal";
+    } else {
+      errors.question = "";
+    }
+    //Validate answer a
+    if (fields.answerA.trim().length < 5) {
+      valid = false;
+      errors.answerA = "Answer A need to be 5 letter minimal";
+    } else {
+      errors.answerA = "";
+    }
+    //Validate answer b
+
+    if (fields.answerB.trim().length < 5) {
+      valid = false;
+      errors.answerB = "Answer Bneed to be 5 letter minimal";
+    } else {
+      errors.answerA = "";
+    }
   };
+
+  if (valid) {
+    console.log("valid!", fields);
+  } else {
+    console.log("validation error!", errors);
+  }
 </script>
 
 <form on:submit|preventDefault={submitHandler}>
@@ -16,6 +50,7 @@
       bind:value={fields.question}
     />
   </div>
+  <span class="error">{errors.question}</span>
 
   <div class="form-field">
     <label for="answer_a">Answer A</label>
@@ -25,6 +60,7 @@
       placeholder="Answer Option A text here"
       bind:value={fields.answerA}
     />
+    <span class="error">{errors.answerA}</span>
   </div>
 
   <div class="form-field">
@@ -35,8 +71,9 @@
       placeholder="Answer Option B text here"
       bind:value={fields.answerB}
     />
+    <span class="error">{errors.answerB}</span>
   </div>
-  <button>Submit</button>
+  <Button type="secondary" flat={false}>Submit</Button>
 </form>
 
 <style>
@@ -58,5 +95,10 @@
   label {
     margin: 10px auto;
     text-align: left;
+  }
+  .error {
+    font-weight: bold;
+    color: red;
+    font-size: 14px;
   }
 </style>
--- src/shared/Button.svelte
+++ src/shared/Button.svelte
@@ -10,11 +10,38 @@
 
 <style>
   button {
+    width: 100%;
     border: 0;
     cursor: pointer;
     border-radius: 6px;
-    padding: 8px 12px;
-    font-weight: bold;
+    padding: 10px 12px;
+    font-weight: 500;
     box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.2);
+  }
+
+  .primary {
+    background-color: #d91b42;
+    color: white;
+  }
+
+  .secondary {
+    background: #45c496;
+    color: white;
+  }
+
+  .flat {
+    box-shadow: none;
+  }
+
+  .primary.inverse {
+    color: #d91b42;
+    background-color: white;
+    border: 2px solid #d91b42;
+  }
+
+  .secondary.inverse {
+    color: #45c496;
+    background: white;
+    border: 2px solid #45c496;
   }
 </style>
